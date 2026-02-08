a782445effe2a69b706d201d2dc87463d2fad6ec
Sahith Akula <akulasahith268@gmail.com>
2026-02-08 22:25:11 +0530

    Added

--- src/components/home/BookingSection.jsx
+++ src/components/home/BookingSection.jsx
@@ -458,7 +458,7 @@ const BookingSection = () => {
                           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
                         </svg>
                         <input
-                          type="text"
+                          type="date"
                           name="pickupDate"
                           value={formData.pickupDate}
                           onChange={handleInputChange}
