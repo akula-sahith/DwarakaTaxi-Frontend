a1b7dc3aa61375db0c0133979e18564a35e94c02
Sahith Akula <akulasahith268@gmail.com>
2026-02-09 19:07:53 +0530

    Added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,9 @@ import HeroSection from "../components/home/HeroSection";
 import Footer from "../components/layout/Footer";
 import BookingSection from "../components/home/BookingSection";
 import TariffSection from "../components/home/CabTypes";
+import About from "./About";
+import ContactSection from "./components/ContactUsComponent";
+import TermsPoliciesComponent from "./components/TermsandConditionsComponent";
 import ServiceAreasBooking from "../components/home/ServiceAreas";
 const HomePage = () => {
   return (
@@ -11,6 +14,9 @@ const HomePage = () => {
       <HeroSection />
       {/* <HeroSection/> */}
       {/* Booking Form Section */}
+      {/* <About/> */}
+      <ContactSection/>
+      <TermsPoliciesComponent/>
       <BookingSection />
       <TariffSection/>
       {/* Footer */}
